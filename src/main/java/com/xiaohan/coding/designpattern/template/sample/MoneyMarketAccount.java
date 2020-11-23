26ee8e5137e52358b8e5bbda1a280558597ad168
hanpanpan <hanpanpan@9fbank.com.cn>
2020-11-23 23:58:10 +0800

    新增设计模式-模板模式

---
+++ src/main/java/com/xiaohan/coding/designpattern/template/sample/MoneyMarketAccount.java
@@ -0,0 +1,13 @@
+package com.xiaohan.coding.designpattern.template.sample;
+
+public class MoneyMarketAccount extends Account {
+    @Override
+    protected String doCalculateAccountType() {
+        return "Money Market";
+    }
+
+    @Override
+    protected double doCalculateInterestRate() {
+        return 0.045;
+    }
+}
